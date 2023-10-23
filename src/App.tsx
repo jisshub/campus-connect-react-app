0d123ce5bc060f8c208eb8e658cfca0c6ee83746
Jissmon Jose <jissmon476@gmail.com>
2023-10-23 17:31:56 +0530

    background of the page should be changed on every page refresh. Do not repeat the color for the next 5 times.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { setUniversities } from './redux/slices/universitySlice';
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     const dispatch = useDispatch();
+     const [bgColor, setBgColor] = useState<string>('');
 
     useEffect(() => {
         axios.get('http://universities.hipolabs.com/search?country=United+States')
@@ -15,10 +16,25 @@ function App() {
                 response.data = response.data.slice(0, 30);
                 dispatch(setUniversities(response.data));
             });
+        const potentialColors = ['#FFDDC1', '#C2CFF0', '#E2FFC6', '#FFEBCD', '#FFD4D4'];
+        let lastColors = JSON.parse(localStorage.getItem('lastColors') || '[]');
+
+        let newColor;
+        do {
+            newColor = potentialColors[Math.floor(Math.random() * potentialColors.length)];
+        } while (lastColors.includes(newColor));
+
+        lastColors.push(newColor);
+        if (lastColors.length > 5) {
+            lastColors.shift();
+        }
+        localStorage.setItem('lastColors', JSON.stringify(lastColors));
+
+        setBgColor(newColor);
     }, [dispatch]);
 
     return (
-        <div>
+        <div style={{ backgroundColor: bgColor, minHeight: '100vh' }}>
             <UniversityForm />
         </div>
     );
